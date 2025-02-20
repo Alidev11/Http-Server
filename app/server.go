840c100815c4220e961adb6bae2bc49b55831f43
Alidev11 <karwawiali@gmail.com>
2025-02-20 16:21:46 +0100

    cleaned the code, have error with respond()

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -8,8 +9,78 @@ import (
 	"strings"
 )
 
+func isUrlValid(url string) bool{
+	switch url{
+	case "/":
+		return true
+	default:
+		return false
+	}
+}
+
+// func connect() net.Listener{
+// 	// Part 1: The server listens on port 4221 using tcp
+	
+// 	return tcpServer
+// }
+
+func getUrlPath(connection net.Conn) (string){
+	// Part 3: extract url from request
+	buffer := make([]byte, 1024)
+		
+	// Read request and store it
+	n, readErr := connection.Read(buffer)
+	if readErr != nil {
+		log.Fatalln("Can't read the request")
+		os.Exit(1)
+	}
+
+	request := string(buffer[:n])
+	
+	// Get the URL path
+	reqSplitted := strings.Split(request, " ")
+	return reqSplitted[1]
+}
+
+func getParam(urlPath string) (string, error){
+	params := strings.Split(urlPath, "/")
+	if len(params)>=3{
+		fmt.Print(params[2])
+		return params[2], nil
+	}
+	return "", errors.New("no parameter found")
+}
+
+func respond(urlPath string, connection net.Conn){
+	parameter, _ := getParam(urlPath)
+	response := ""
+	
+	if parameter == ""{
+		if !isUrlValid(urlPath){
+			response = "HTTP/1.1 404 NOT FOUND"
+		}else{
+			response = "HTTP/1.1 200 OK"
+		}
+	}else{
+		response = respondParameter(parameter)
+	}
+
+	_, writeErr := connection.Write([]byte(response))
+	if writeErr != nil {
+		log.Fatalln("Can't write data to the client!")
+		os.Exit(1)
+	}	
+}
+
+func respondParameter(parameter string) (string){
+	return fmt.Sprintf(
+		"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+		len(parameter), parameter,
+	)
+}
+
 func main() {
-	// Part 1: The server listens on port 4221 using tcp
+	// Establish connection with client
 	tcpServer, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		log.Fatalln("Found error when creating the server")
@@ -17,9 +88,7 @@ func main() {
 	}
 	defer tcpServer.Close()
 	log.Println("Server listening on port 4221...")
-
 	
-	// Part 2: handle client connection to the server
 	for{
 		// accept the client connection and write a reply to the client
 		conn, err := tcpServer.Accept()
@@ -27,35 +96,12 @@ func main() {
 			log.Fatalln("Can't connect to server!")
 		}
 
-		// Part 3: extract url from request
-		buffer := make([]byte, 1024)
-		
-		// Read request and store it
-		n, readErr := conn.Read(buffer)
-		if readErr != nil {
-			log.Fatalln("Can't read the request")
-		}
-		request := string(buffer[:n])
-		
-		// Get the URL path
-		reqSplitted := strings.Split(request, " ")	
-		fmt.Println("url path:", reqSplitted[1])
-		
-		// Get the parameter
-		params := strings.Split(reqSplitted[1], "/")
-		fmt.Println(params[2])
-		fmt.Println(len(params[2]))
+		// get url path
+		urlPath := getUrlPath(conn)
+		// fmt.Println("url path:", urlPath)
 
-		// return the response
-		response := fmt.Sprintf(
-			"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-			len(params[2]), params[2],
-		)
-
-		_, writeErr := conn.Write([]byte(response))
-		if writeErr != nil {
-			log.Fatalln("Can't write data to the client!")
-		}
+		// return the response and close connection
+		respond(urlPath, conn)
 		conn.Close()
 	}	
 }
