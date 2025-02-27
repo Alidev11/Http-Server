40e6ee09ebcfde4d914e4572f42a68fdf990fb70
Alidev11 <karwawiali@gmail.com>
2025-02-27 19:21:01 +0000

    added /user-agent endpoint successfully

--- app/utils.go
+++ app/utils.go
@@ -1,38 +1,19 @@
 package main
 
-import(
-	"strings"
-	"fmt"
+import (
 	"errors"
-	"net"
+	"fmt"
 	"log"
+	"net"
 	"os"
+	"strings"
 )
 
-// checks if url is in the server
-func IsUrlValid(url string) bool{
-	switch url{
-	case "/":
-		return true
-	default:
-		return false
-	}
-}
-
-func GetParam(urlPath string) (string, error){
-	params := strings.Split(urlPath, "/")
-	if len(params)>=3{
-		fmt.Print(params[2])
-		return params[2], nil
-	}
-	return "", errors.New("no parameter found")
-}
-
-func GetUrlPath(connection net.Conn) string{
-	// Part 3: extract url from request
+// ************** Request Methods ********************
+func ParseRequest(connection net.Conn) map[int]string{
 	buffer := make([]byte, 1024)
+	var m map[int]string = make(map[int]string)
 		
-	// Read request and store it
 	n, readErr := connection.Read(buffer)
 	if readErr != nil {
 		log.Fatalln("Can't read the request")
@@ -40,35 +21,70 @@ func GetUrlPath(connection net.Conn) string{
 	}
 
 	request := string(buffer[:n])
-	
-	// Get the URL path
-	reqSplitted := strings.Split(request, " ")
-	return reqSplitted[1]
+	reqSplitted := strings.Split(request, "\r\n")
+
+	for i, value := range reqSplitted{
+		m[i] = value
+	}
+	fmt.Println(m)
+
+	return m
 }
 
-func RespondParameter(parameter string) (string){
-	return fmt.Sprintf(
-		"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-		len(parameter), parameter,
-	)
+func GetUrlPath(parsedReq map[int]string) string{
+	requestLine := parsedReq[0]
+	urlPath := strings.Split(requestLine, " ")[1]
+	return urlPath
 }
 
-// main function to return a response
-func Respond(urlPath string, connection net.Conn){
+func GetParam(urlPath string) (string, error){
+	params := strings.Split(urlPath, "/")
+	if len(params)>=3{
+		return params[2], nil
+	}
+	return "", errors.New("no paramet found")
+}
+
+func ReadHeader(parsedReq map[int]string) (string){
+	for _, value := range parsedReq{
+		userAgentHeader, cond := strings.CutPrefix(value, "User-Agent: ")
+		// fmt.Printf(userAgentHeader)
+		fmt.Println(cond)
+		if cond{
+			return fmt.Sprintf(
+				"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+				len(userAgentHeader), userAgentHeader,
+			)
+		}
+	}
+
+	return ""
+}
+
+// *************** Response Methods ********************
+
+func Respond(urlPath string, parsedReq map[int]string){
 	parameter, _ := GetParam(urlPath)
 	response := ""
 	
 	if parameter == ""{
-		if !IsUrlValid(urlPath){
-			response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
-		}else{
-			response = "HTTP/1.1 200 OK\r\n\r\n"
+		switch urlPath{
+			case "/":
+				response = "HTTP/1.1 200 OK\r\n\r\n"
+			case "/user-agent":
+				response = ReadHeader(parsedReq)
+			default:
+				fmt.Println("Default response")
+				response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
 		}
 	}else{
-		response = RespondParameter(parameter)
+		response = fmt.Sprintf(
+			"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+			len(parameter), parameter,
+		)
 	}
 
-	_, writeErr := connection.Write([]byte(response))
+	_, writeErr := Conn.Write([]byte(response))
 	if writeErr != nil {
 		log.Fatalln("Can't write data to the client!")
 		os.Exit(1)
